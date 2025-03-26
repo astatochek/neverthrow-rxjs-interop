bf899e8c933f7433d630095fd19e8f3f1a971f10
Ostap Korotchenok <o.korotchenok@yadro.com>
2025-03-26 23:56:39 +0300

    - add ut

--- index.ts
+++ index.ts
@@ -1,24 +1,17 @@
 import { err, ok, Result } from "neverthrow";
-import {
-  catchError,
-  map,
-  Observable,
-  of,
-  switchMap,
-  type UnaryFunction,
-} from "rxjs";
+import { map, Observable, of, switchMap, type UnaryFunction } from "rxjs";
 
 type Prettify<T> = {
   [K in keyof T]: T[K];
 } & {};
 
-function mapResult<T, U, E = Error>(
+export function mapResult<T, U, E>(
   project: (v: T) => U,
 ): UnaryFunction<Observable<Result<T, E>>, Observable<Result<U, E>>> {
   return map((r) => r.map(project));
 }
 
-function switchMapResult<T, U, E1 = Error, E2 = Error>(
+export function switchMapResult<T, U, E1 = Error, E2 = Error>(
   project: (v: T) => Observable<Result<U, E2>>,
 ): UnaryFunction<Observable<Result<T, E1>>, Observable<Result<U, E1 | E2>>> {
   return switchMap((r) => {
@@ -30,39 +23,55 @@ function switchMapResult<T, U, E1 = Error, E2 = Error>(
   });
 }
 
-type ExtractErrFromSource<
+type ExtractErrFromRecordSource<
   T extends Record<string, Observable<Result<any, any>>>,
 > = T[keyof T] extends Observable<Result<any, infer E>> ? E : never;
-
-type ExtractOkFromSource<
+type ExtractOkFromRecordSource<
   T extends Record<string, Observable<Result<any, any>>>,
 > = {
   [Key in keyof T]: T[Key] extends Observable<Result<infer O, any>> ? O : never;
 };
+type ExtractErrFromArraySource<T extends Observable<Result<any, any>>[]> =
+  T[number] extends Observable<Result<any, infer E>> ? E : never;
+type ExtractOkFromArraySource<T extends Observable<Result<any, any>>[]> =
+  T[number] extends Observable<Result<infer O, any>> ? O[] : never;
 
-function forkJoinResult<T extends Record<string, Observable<Result<any, any>>>>(
+export function forkJoinResult<
+  T extends Record<string, Observable<Result<any, any>>>,
+>(
   sources: T,
-): Observable<Result<Prettify<ExtractOkFromSource<T>>, ExtractErrFromSource<T>>> {
-  const entries = Object.entries(sources) as [
+): Observable<
+  Result<Prettify<ExtractOkFromRecordSource<T>>, ExtractErrFromRecordSource<T>>
+>;
+export function forkJoinResult<T extends Observable<Result<any, any>>[]>(
+  sources: T,
+): Observable<
+  Result<ExtractOkFromArraySource<T>, ExtractErrFromArraySource<T>>
+>;
+export function forkJoinResult<T>(sources: T): any {
+  const entries = Object.entries(sources as any) as [
     string,
     Observable<Result<any, any>>,
   ][];
 
+  const isArray = Array.isArray(sources);
+
   return new Observable((subscriber) => {
-    const acc: Record<string, any> = {};
+    const acc: Record<string, any> = isArray ? [] : {};
 
     let pending = entries.length;
 
     // handle empty object
     if (pending === 0) {
-      subscriber.next(ok({} as any));
+      subscriber.next(ok(acc));
       subscriber.complete();
       return;
     }
 
     for (const [key, obs$] of entries) {
-      obs$.pipe(catchError((error) => of(err(error)))).subscribe({
-        next: (result) => {
+      obs$.subscribe(
+        //eslint-disable-next-line no-loop-func
+        (result) => {
           if (result.isErr()) {
             subscriber.next(result);
             subscriber.complete();
@@ -74,39 +83,11 @@ function forkJoinResult<T extends Record<string, Observable<Result<any, any>>>>(
           pending--;
 
           if (pending === 0) {
-            subscriber.next(ok(acc) as any);
+            subscriber.next(ok(acc));
             subscriber.complete();
           }
         },
-      });
+      );
     }
   });
 }
-
-// TEST
-
-function getFoo() {
-  return of(Math.random() > 0.5 ? ok("foo ok" as const) : err("foo:err"));
-}
-
-function getBar() {
-  return of(Math.random() > 0.5 ? ok("bar ok" as const) : err("bar:err"));
-}
-
-forkJoinResult({ foo: getFoo(), bar: getBar() }).subscribe((res) => {
-  if (res.isOk()) {
-    console.log("ok:", res.value);
-  } else {
-    console.error("err:", res.error);
-  }
-});
-
-// getFoo()
-//   .pipe(switchMapResult(() => getBar()))
-//   .subscribe((res) => {
-//     if (res.isOk()) {
-//       console.log("ok:", res.value);
-//     } else {
-//       console.error("err:", res.error);
-//     }
-//   });
